da7671539c08ae8564c3e9b763d27ad99033eaa7
Brad <93143095+brad-ktech@users.noreply.github.com>
2022-01-21 09:51:22 -0600

    Add files via upload

---
+++ src/test/java/com/ktech/starter/KtechSpringAwsStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktech.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtechSpringAwsStarterApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
