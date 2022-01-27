dbb5eb9deed1b836fe4fc6834ccabbd8cc029b73
brad-ktech <brad@ktech.dev>
2022-01-27 13:01:16 -0600

    CalendarAPI refactor

--- src/main/java/com/ktech/starter/clio/apis/AbstractRestAPI.java
+++ src/main/java/com/ktech/starter/clio/apis/AbstractRestAPI.java
@@ -1,16 +1,34 @@
 package com.ktech.starter.clio.apis;
 
+import com.google.gson.Gson;
 import com.ktech.starter.annotations.ApiFields;
 import com.ktech.starter.annotations.ApiPath;
+import com.ktech.starter.clio.messages.Result;
 import com.ktech.starter.clio.models.CalendarEntry;
+import com.ktech.starter.clio.models.Request;
 import com.ktech.starter.vaults.ClioConfigurationVault;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 public class AbstractRestAPI {
 
+    @Autowired
     protected ClioConfigurationVault vault;
 
-    protected String encodeFields(String aFieldString) {
+   protected String encodeFields(String aFieldString) {
         return aFieldString.replace("{", "%7B").replace("}", "%7D");
     }
 
@@ -40,6 +58,43 @@ public class AbstractRestAPI {
 
 
     }
+
+    protected <T> void doPost(T t) throws IOException {
+System.out.println("in do post");
+        try(CloseableHttpClient client = HttpClients.createDefault()) {
+
+            HttpPost post = new HttpPost(getUrl(t.getClass()));
+            post.setHeaders(getBasicHeaders());
+            Request<T> request = new Request<>(t);
+            post.setEntity(new StringEntity(new Gson().toJson(request)));
+            System.out.println("after post entity");
+
+            HttpResponse response = client.execute(post);
+            HttpEntity entity = response.getEntity();
+            String json = IOUtils.toString(entity.getContent(), "UTF-8");
+            Result<CalendarEntry> result = new Gson().fromJson(json, Result.class);
+
+            System.out.println("[STATUS] " + response.getStatusLine().getStatusCode() + " " + response.getStatusLine().getReasonPhrase());
+
+        }
+
+
+
+
+    }
+
+    private Header[] getBasicHeaders(){
+
+        Header[] headers = new Header[2];
+        headers[0] = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        headers[1] = new BasicHeader("Authorization", "Bearer " + vault.getAuthToken());
+        return headers;
+
+    }
+
+
+
+
 
     protected <T> String getUrl(Class<T> clazz){
 
--- src/main/java/com/ktech/starter/clio/apis/CalendarAPI.java
+++ src/main/java/com/ktech/starter/clio/apis/CalendarAPI.java
@@ -7,65 +7,29 @@ import com.ktech.starter.clio.models.CalendarEntry;
 import com.ktech.starter.clio.models.Request;
 import com.ktech.starter.vaults.ClioConfigurationVault;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.*;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.*;
-import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.HttpParams;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class CalendarAPI extends AbstractRestAPI{
 
 
-    private RestTemplate rest;
-
-    public CalendarAPI(@Autowired ClioConfigurationVault vault, @Autowired RestTemplate rest){
-
-        this.vault = vault;
-        this.rest = rest;
+    public <T> void saveCalendarEntry(CalendarEntry ce) throws IOException, URISyntaxException {
 
 
-    }
-
-    public <T> CalendarEntry saveCalendarEntry(CalendarEntry ce) throws IOException, URISyntaxException {
-
-
-        try(CloseableHttpClient client = HttpClients.createDefault()) {
-
-            HttpPost post = new HttpPost(getUrl(ce.getClass()));
-            post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-            post.setHeader("Authorization", "Bearer " + vault.getAuthToken());
-
-            Request<CalendarEntry> request = new Request<>(ce);
-            post.setEntity(new StringEntity(new Gson().toJson(request)));
-
-
-            HttpResponse response = client.execute(post);
-            HttpEntity entity = response.getEntity();
-            String json = IOUtils.toString(entity.getContent(), "UTF-8");
-            Result<CalendarEntry> result = new Gson().fromJson(json, Result.class);
-
-            System.out.println(response.getStatusLine().getReasonPhrase());
-            System.out.println("[STATUS] " + response.getStatusLine().getStatusCode());
-            return result.getData();
-        }
+        System.out.println("in CalendarAPI");
+            doPost(ce);
 
     }
 
